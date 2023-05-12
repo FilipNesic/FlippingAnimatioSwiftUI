ced693f4de0357e17a0d99697bac2dfdeb708457
FilipNesic <110664188+FilipNesic@users.noreply.github.com>
2023-05-12 10:53:38 +0200

    Initial Commit

---
+++ FlippingAnimationSwiftUI/FlippingAnimationSwiftUI/ContentView.swift
@@ -0,0 +1,40 @@
+//
+//  ContentView.swift
+//  FlippingAnimationSwiftUI
+//
+
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var flipCoin: Bool = false
+    private var coinImage = ["Head", "Tail"]
+    
+    var body: some View {
+        VStack {
+            if flipCoin {
+                coin
+            } else {
+                coin
+            }
+        }
+        .rotation3DEffect(Angle(degrees: flipCoin ? 1080 : 0), axis: (x: 1, y: 0, z: 0))
+        .onTapGesture {
+            withAnimation(.easeIn(duration: 0.5)) {
+                flipCoin.toggle()
+            }
+        }
+    }
+    
+    private var coin: some View { 
+        Image(coinImage.randomElement()!)
+            .resizable()
+            .frame(width: 250, height: 250)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FlippingAnimationSwiftUI/FlippingAnimationSwiftUI/FlippingAnimationSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlippingAnimationSwiftUIApp.swift
+//  FlippingAnimationSwiftUI
+//
+//  Created by Filip  on 11.5.23..
+//
+
+import SwiftUI
+
+@main
+struct FlippingAnimationSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
